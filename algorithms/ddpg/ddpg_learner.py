6556796730b55296bb768474b504e6e249491716
侯志民 <houzhimin@houzhimindeMacBook-Pro.local>
2019-09-15 10:50:08 +0800

    update

--- algorithms/ddpg/ddpg_learner.py
+++ algorithms/ddpg/ddpg_learner.py
@@ -26,13 +26,13 @@ def denormalize(x, stats):
 
 
 def reduce_std(x, axis=None, keepdims=False):
-    return tf.sqrt(reduce_var(x, axis=axis, keepdims=keepdims))
+    return tf.sqrt(reduce_var(x, axis=axis, keep_dims=keepdims))
 
 
 def reduce_var(x, axis=None, keepdims=False):
-    m = tf.reduce_mean(x, axis=axis, keepdims=True)
+    m = tf.reduce_mean(x, axis=axis, keep_dims=True)
     devs_squared = tf.square(x - m)
-    return tf.reduce_mean(devs_squared, axis=axis, keepdims=keepdims)
+    return tf.reduce_mean(devs_squared, axis=axis, keep_dims=keepdims)
 
 
 def get_target_updates(vars, target_vars, tau):
